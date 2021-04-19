0a6a45eb32f64a186a13fe48180a57316ee4e5b8
rick-martin-unifyconsulting <68446606+rick-martin-unifyconsulting@users.noreply.github.com>
2021-04-19 13:26:09 -0700

    Consolidated MorningFreshMember and NonMorningFreshMember into MorningFreshCustomer with isMember flag

--- src/test/java/com/unifyconsulting/superSmoothies/SuperSmoothiesSteps.java
+++ src/test/java/com/unifyconsulting/superSmoothies/SuperSmoothiesSteps.java
@@ -11,14 +11,13 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class SuperSmoothiesSteps {
 
-    private DrinkCatalog drinkCatalog = new DrinkCatalog();
-    private SuperSmoothiesSchema superSmoothieSchema = new SuperSmoothiesSchema(drinkCatalog);
-    private MorningFreshMember morningFreshMember;
+    private final DrinkCatalog drinkCatalog = new DrinkCatalog();
+    private final SuperSmoothiesSchema superSmoothieSchema = new SuperSmoothiesSchema(drinkCatalog);
     private iCustomer customer;
 
     @Given("the following drink categories:")
     public void the_following_drink_categories(List<Map<String, String>> drinkCategories) {
-        drinkCategories.stream().forEach( drinkCategory -> {
+        drinkCategories.forEach(drinkCategory -> {
             String drink = drinkCategory.get("Drink");
             String category = drinkCategory.get("Category");
             Integer points = Integer.parseInt(drinkCategory.get("Points"));
@@ -29,13 +28,15 @@ public class SuperSmoothiesSteps {
 
     @Given("^(.*) is a Morning Freshness member$")
     public void customer_is_a_morning_freshness_member(String name) {
-        customer = new MorningFreshMember(name, superSmoothieSchema);
+        customer = new MorningFreshCustomer(name, superSmoothieSchema, true);
     }
 
     @When("^(.*) purchases (\\d+) (.*) drinks?")
     public void member_purchases_apple_and_kale_drinks(String name,
                                                         Integer quantity,
                                                         String drink) {
+
+        System.out.println(String.format("### Customer %s is ordering %d %s",name, quantity, drink));
         customer.orders(quantity, drink);
     }
 
@@ -46,7 +47,7 @@ public class SuperSmoothiesSteps {
 
     @Given("^(.*) is not a Morning Freshness member")
     public void jack_is_not_a_morning_freshness_member(String name) {
-        customer = new NonMorningFreshMember(name,superSmoothieSchema);
+        customer = new MorningFreshCustomer(name,superSmoothieSchema, false);
     }
 
 }
